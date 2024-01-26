8d091ed08633aa6a92359ea621983db0ca550b37
Branden97 <1894876+Branden97@users.noreply.github.com>
2024-01-26 16:06:27 -0500

    feat: increase paralax effect
    make stars circular
    blur stars

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import "./assets/styles.sass"
 import { StarBackground } from "./components/StarBackground"
 
 function App() {
+  const starMotionPixelsX = 100
+  const starMotionPixelsY = 20
+
   return (
     <>
       <h1>
@@ -9,9 +12,21 @@ function App() {
       </h1>
 
       <div id="star-container">
-        <StarBackground className="small" maxXOffset={10} maxYOffset={5} />
-        <StarBackground className="medium" maxXOffset={20} maxYOffset={15} />
-        <StarBackground className="large" maxXOffset={30} maxYOffset={25} />
+        <StarBackground
+          className="small"
+          maxXOffset={starMotionPixelsX * 0.3}
+          maxYOffset={starMotionPixelsY * 0.3}
+        />
+        <StarBackground
+          className="medium"
+          maxXOffset={starMotionPixelsX * 0.6}
+          maxYOffset={starMotionPixelsY * 0.6}
+        />
+        <StarBackground
+          className="large"
+          maxXOffset={starMotionPixelsX * 1}
+          maxYOffset={starMotionPixelsY * 1}
+        />
       </div>
       {/* <div className="middle" /> */}
     </>

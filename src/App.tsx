38ed096d65d368791c90e7dd33fb5634751db6f1
Branden97 <1894876+Branden97@users.noreply.github.com>
2024-01-26 12:48:59 -0500

    fix: make star-background not jump with mouse
    make title's gradient uniform on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import "./assets/styles.sass";
-import { StarBackground } from "./components/StarBackground";
+import "./assets/styles.sass"
+import { StarBackground } from "./components/StarBackground"
 
 function App() {
   return (
     <>
       <h1>
-        Branden <span>Williams</span>
+        <span>Branden</span> <span>Williams</span>
       </h1>
 
       <div id="star-container">
-        <StarBackground className="small" maxXOffset={10} maxYOffset={10} />
-        <StarBackground className="medium" maxXOffset={20} maxYOffset={20} />
-        <StarBackground className="large" maxXOffset={30} maxYOffset={30} />
+        <StarBackground className="small" maxXOffset={10} maxYOffset={5} />
+        <StarBackground className="medium" maxXOffset={20} maxYOffset={15} />
+        <StarBackground className="large" maxXOffset={30} maxYOffset={25} />
       </div>
       {/* <div className="middle" /> */}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/StarBackground.tsx
+++ src/components/StarBackground.tsx
@@ -1,45 +1,65 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react"
 // import styles from "../assets/stars.sass";
 
-import "../assets/star-background.sass";
+import "../assets/star-background.sass"
 
 interface StarBackgroundProps {
-  className: string;
-  maxXOffset: number;
-  maxYOffset: number;
+  className: string
+  maxXOffset: number
+  maxYOffset: number
 }
 
-export function StarBackground({
-  className,
-  maxXOffset,
-  maxYOffset,
-}: StarBackgroundProps) {
-  function parallax(event: TouchEvent | MouseEvent) {
-    const clientX =
-      "touches" in event ? event.touches[0].clientX : event.clientX;
-    const clientY =
-      "touches" in event ? event.touches[0].clientY : event.clientY;
+export function StarBackground({ className, maxXOffset, maxYOffset }: StarBackgroundProps) {
+  const [initialNormalizedEntryPoint, setInitialNormalizedEntryPoint] = useState<{
+    x: null | number
+    y: null | number
+  }>({
+    x: null,
+    y: null,
+  })
 
-    const xModifier = (clientX / window.innerWidth - 0.5) * 2; // left: -1, right: 1
-    const calculatedXOffset = maxXOffset * xModifier * -1;
-    const yModifier = (clientY / window.innerWidth - 0.5) * 2; // top: -1, bottom: 1
-    const calculatedYOffset = maxYOffset * yModifier * -1;
-    const smallStars = document.querySelector<HTMLElement>(
-      `.star-background.${className}`
-    );
-    if (!smallStars) return;
-    // smallStars.style.left = `calc(50vw + ${calculatedXOffset}px)`;
-    smallStars.style.marginLeft = `${calculatedXOffset}px`;
-    smallStars.style.marginTop = `${calculatedYOffset}px`;
+  function parallax(event: TouchEvent | MouseEvent) {
+    const clientX = "touches" in event ? event.touches[0].clientX : event.clientX
+    const clientY = "touches" in event ? event.touches[0].clientY : event.clientY
+
+    function getNormalizedValue(value: number, maxValue: number): number {
+      return (value - maxValue / 2) / (maxValue / 2)
+    }
+
+    if (initialNormalizedEntryPoint.x === null || initialNormalizedEntryPoint.y === null) {
+      setInitialNormalizedEntryPoint({ x: getNormalizedValue(clientX, window.innerWidth), y: getNormalizedValue(clientY, window.innerHeight) })
+    }
+    if (initialNormalizedEntryPoint.x === null || initialNormalizedEntryPoint.y === null) {
+      return
+    }
+
+    const xModifier = getNormalizedValue(clientX, window.innerWidth) - initialNormalizedEntryPoint.x
+    const calculatedXOffset = maxXOffset * xModifier * -1
+
+    const yModifier = getNormalizedValue(clientY, window.innerHeight) - initialNormalizedEntryPoint.y
+    const calculatedYOffset = maxYOffset * yModifier * -1
+
+    const smallStars = document.querySelector<HTMLElement>(`.star-background.${className}`)
+    if (!smallStars) {
+      console.error("Small stars element not found")
+      return
+    }
+
+    smallStars.style.marginLeft = `${calculatedXOffset}px`
+    smallStars.style.marginTop = `${calculatedYOffset}px`
   }
 
   useEffect(() => {
-    document.addEventListener("mousemove", parallax);
-    document.addEventListener("touchmove", parallax);
+    console.log("Adding event listeners for mousemove and touchmove")
+    document.addEventListener("mousemove", parallax)
+    document.addEventListener("touchmove", parallax)
+
     return () => {
-      document.removeEventListener("mousemove", parallax);
-      document.removeEventListener("touchmove", parallax);
-    };
-  }, []);
-  return <div className={`star-background ${className}`} />;
+      console.log("Removing event listeners for mousemove and touchmove")
+      document.removeEventListener("mousemove", parallax)
+      document.removeEventListener("touchmove", parallax)
+    }
+  }, [initialNormalizedEntryPoint])
+
+  return <div className={`star-background ${className}`} />
 }
